555b66f1df132063fb8566fb0b5efaa7d10e8ab7
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-09-12 14:50:10 +0530

    feat: add fuction to solving matches-extra-runs-conceded-per-team

---
+++ server/matches-extra-runs-conceded-per-team.js
@@ -0,0 +1,56 @@
+const matches = require("../data/jsonData/matches.json");
+const deliveries = require("../data/jsonData/deliveries.json");
+const fs = require("fs");
+//Extra runs conceded per team in the year 2016
+
+function getExtraRunsByTeamInaSeason(season){
+
+      if(season===undefined|| season===null){
+        return "Please provide the season";
+      }
+    let extraRunsByTeam = {};               // creating the object
+
+    let matchIds = new Set();
+
+    for(let index=0;index<matches.length;index++){                       // iterate over all matches
+        let presentSeason = matches[index].season;            // getting the season of particular match
+        if(presentSeason===season){                       // if season should equal to given season
+            let match_id = matches[index].id;  
+            matchIds.add(match_id);                   // pushing matchid in matchIds array
+        } 
+
+    }
+  
+
+     // checking if the seson is matching or not
+         
+            for(let j=0;j<deliveries.length;j++){
+                let matches_id = deliveries[j].match_id;
+                if(matchIds.has(matches_id)){                       // checking id and match_id are matching or not 
+                    let bowlingTeam = deliveries[j].bowling_team;
+                    if(!extraRunsByTeam.hasOwnProperty(bowlingTeam)){     // checking the bowlingTeam is in the team
+                        extraRunsByTeam[bowlingTeam]=0;
+                    }
+                    let matchExtraRuns = Number(deliveries[j].extra_runs);   // converting them into numbers and add to bowling team
+                    extraRunsByTeam[bowlingTeam]+=matchExtraRuns;
+
+                   /* let matchWideRuns = Number(deliveries[j].wide_runs);   // here wide and no ball runs should be consider if extraruns not inclued
+                    extraRuns[bowlingTeam]+=matchWideRuns;
+
+                    let matchNoBallRuns = Number(deliveries[j].noball_runs);
+                    extraRuns[bowlingTeam]+=matchNoBallRuns;*/
+
+                }
+            }
+           
+            return extraRunsByTeam;  // returing the extraRuns object
+        }
+    
+const extraRunsByTeam = getExtraRunsByTeamInaSeason(2016);
+
+fs.writeFileSync("../public/output/matchesExtraRuns.json",JSON.stringify(extraRunsByTeam,null,2)); // written the output in the mention path
+
+
+module.exports = {getExtraRunsByTeamInaSeason};
+
+
