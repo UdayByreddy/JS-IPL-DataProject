869106ed731bde51743bdfbe24c1586c68445e25
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-09-23 14:52:26 +0530

    fix: add initialization function to highest-number-time-player-dismissed-player.js

--- src/server/highest-number-time-player-dismissed-player.js
+++ src/server/highest-number-time-player-dismissed-player.js
@@ -5,7 +5,6 @@ function getHighestNumberOfTimesDismissedPlayers(deliveries){
     let playersList = {};                // created an object
     
         for(let j=0;j<deliveries.length;j++){         // iterate the deliveries data
-            let matchId = deliveries[j].match_id;
                 let dismissed  = deliveries[j].player_dismissed;
                 if(dismissed!==null){                       // check only dismissed is not null
                     let bowlerName = deliveries[j].bowler;
